b6261343ebfef714d41162850d75cce272e15b1a
isaac <isaacjcann@gmail.com>
2024-01-16 20:03:28 +1100

    feat: static file server

---
+++ README.md
@@ -0,0 +1,8 @@
+# Personal Website
+This repo exist purely for those interesting
+in mimicking my personal [website](https://cann.page).
+There are no guarantees of quality.
+
+## Current Form:
+A simple static file server using Axum. <br>
+Based on their provided [examples](https://github.com/tokio-rs/axum/blob/main/examples/static-file-server/src/main.rs).
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
-use axum::{response, routing, Router};
+use axum::Router;
+use tower_http::services;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", routing::get(handler));
+    let serve_dir = services::ServeDir::new("assets")
+        .not_found_service(services::ServeFile::new("assets/index.html"));
+    let app = Router::new().fallback_service(serve_dir);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     println!("listen on {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
-}
-
-async fn handler() -> response::Html<&'static str> {
-    response::Html("<h1> hello world </h1>")
 }
